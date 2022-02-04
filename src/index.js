c3b4118c6480319d2563701db12acb5b78de8dac
Michael Claborn <mclaborn2010@gmail.com>
2022-02-03 22:05:29 -0600

    started app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 // Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
 
-// Create a react component
+// Create a React component
+const App = () => {
+  return <div>Hi There</div>;
+};
+
+// Take the React component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
